d7e3b22cef1e017d71827397875d31867f71c6ac
danielkamen <kamen.d@northeastern.edu>
2023-01-18 13:09:32 -0500

    updated hw again after relinking vscode

--- exercises/hello/hello.cpp
+++ exercises/hello/hello.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main(void)
 {
- cout<<"Hello world! "<<endl;
+ cout<<"|) /\ |\| | [- |_   /< /\ |\/| [- |\| - Daniel Kamen:"<<endl;
  //cout<<"Hello world! "+to_string(123)<<endl;
 }
