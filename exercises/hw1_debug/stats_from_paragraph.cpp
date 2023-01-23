38191dd659933da73b982c74401db1dfce9c843e
danielkamen <kamen.d@northeastern.edu>
2023-01-23 16:53:37 -0500

    final touches

--- exercises/hw1_debug/stats_from_paragraph.cpp
+++ exercises/hw1_debug/stats_from_paragraph.cpp
@@ -12,7 +12,7 @@ int main() {
     int words = 0, consonants = 0, vowels = 0, includeSpecial = 0, startWithVowel = 0;
 
     // paragraph input data
-    cout << "Please enter a paragraph. Press [Enter] when you are done." << endl;
+    cout << "Please enter a paragraph. Press [Enter] when you are done."<< "PLEASE DONT USE A DOUBLE SPACE :)" << endl;
     getline(cin, paragraph);
     if (paragraph.empty()) {
         cout << "Error! Paragraph name is required.\n";
