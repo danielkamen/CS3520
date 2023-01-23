cdfa90eab91a04fa4eb1c583c3bc4837782b6a4f
danielkamen <kamen.d@northeastern.edu>
2023-01-23 11:35:26 -0500

    lecture code + changes in hw

--- exercises/hw1_debug/find_and_replace.cpp
+++ exercises/hw1_debug/find_and_replace.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-int main(){
+int main() {
 
     //delcares variable
     string paragraph;
@@ -13,66 +13,66 @@ int main(){
     int count = 0;
     int position = 0;
     vector<int> posOfWords(0);
-    
 
-    cout<<"Please enter a paragraph. Press [Enter] when you are done."<<endl;
+
+    cout << "Please enter a paragraph. Press [Enter] when you are done." << endl;
     getline(cin, paragraph);
-    if (paragraph.empty()) { 
+    if (paragraph.empty()) {
         cout << "Error! Paragraph name is required.\n";
     }
     int lenParagraph = paragraph.size();
 
-    cout<<"Please type a word you want to find: ";
-    cin>>goalWord;
-    if (goalWord.size() < 2) { 
+    cout << "Please type a word you want to find: ";
+    cin >> goalWord;
+    if (goalWord.size() < 2) {
         cout << "Error! Longer word is required.\n";\
     }
     int wordLength = goalWord.size();
-   
+
     vector<int> posOfSpace(0);
     posOfSpace.push_back(0);
-    for(int i = 0; i<= lenParagraph; i++){
-        if(paragraph[i] == ' '){
+    for (int i = 0; i <= lenParagraph; i++) {
+        if (paragraph[i] == ' ') {
             posOfSpace.push_back(i);
         }
     }
     posOfSpace.push_back(lenParagraph);
-    
 
-    for(int i = 0; i<posOfSpace.size() - 1; i++){
-        if(i == 0) {
+
+    for (int i = 0; i < posOfSpace.size() - 1; i++) {
+        if (i == 0) {
             string tempWord = paragraph.substr(0, (posOfSpace.at(1)));
-            if(tempWord == goalWord){
-         count++;
-         cout<<"Position of the word: "<< 0 <<endl;
-         posOfWords.push_back(posOfSpace.at(i));
-        }
+            if (tempWord == goalWord) {
+                count++;
+                cout << "Position of the word: " << 0 << endl;
+                posOfWords.push_back(posOfSpace.at(i));
+            }
         }
         else {
-        string tempWord = paragraph.substr(posOfSpace.at(i) + 1, (posOfSpace.at(i+1)- (posOfSpace.at(i) + 1)));
-        if(tempWord == goalWord){
-         count++;
-         cout<<"Position of the word: "<< posOfSpace.at(i) + 1 <<endl;
-        posOfWords.push_back(posOfSpace.at(i) + 1 );
-        }
+            string tempWord = paragraph.substr(posOfSpace.at(i) + 1, (posOfSpace.at(i + 1) - (posOfSpace.at(i) + 1)));
+            if (tempWord == goalWord) {
+                count++;
+                cout << "Position of the word: " << posOfSpace.at(i) + 1 << endl;
+                posOfWords.push_back(posOfSpace.at(i) + 1);
+            }
         }
     }
-    cout<<"Total number of times the word was found: "<< count<<endl;
-    
-    cout<<"Please type another word to replace the word you sought to find: ";
-    cin>>newWord;
+    cout << "Total number of times the word was found: " << count << endl;
+
+    cout << "Please type another word to replace the word you sought to find: ";
+    cin >> newWord;
 
     int lengthOfNewWord = newWord.size();
     int difInSizes = lengthOfNewWord - wordLength;
-    for(int i = 0; i < posOfWords.size(); i++){
+    for (int i = 0; i < posOfWords.size(); i++) {
         paragraph.erase(posOfWords.at(i), wordLength);
         paragraph.insert(posOfWords.at(i), newWord);
-        for(int j = 1; j< posOfWords.size(); j++){
+        for (int j = 1; j < posOfWords.size(); j++) {
             posOfWords[j] = posOfWords.at(j) + difInSizes;
         }
     }
-    cout<<"Here is the new paragraph\n";
-    cout<<paragraph<<endl;
+    cout << "Here is the new paragraph\n";
+    cout << paragraph << endl;
 
     return 0;
 
--- exercises/hw1_debug/stats_from_paragraph..cpp
+++ exercises/hw1_debug/stats_from_paragraph..cpp
@@ -5,113 +5,124 @@
 
 using namespace std;
 
-int main(){
+int main() {
 
     //delcares variable
     string paragraph;
     int words = 0, consonants = 0, vowels = 0, includeSpecial = 0, startWithVowel = 0;
 
-    cout<<"Please enter a paragraph. Press [Enter] when you are done."<<endl;
+    cout << "Please enter a paragraph. Press [Enter] when you are done." << endl;
     getline(cin, paragraph);
-    if (paragraph.empty()) { 
+    if (paragraph.empty()) {
         cout << "Error! Paragraph name is required.\n";
-}
+    }
     int lenParagraph = paragraph.size();
-    cout<<"What you typed:"<<paragraph<<endl;
-    for(int i = 0; i<= lenParagraph; i++){
-        if(paragraph[i] == ' '){
+    cout << "What you typed:" << paragraph << endl;
+    for (int i = 0; i <= lenParagraph; i++) {
+        if (paragraph[i] == ' ') {
             words++;
         }
     }
     words++;
-    cout<<"Number of words typed: "<<words<<endl;
+    cout << "Number of words typed: " << words << endl;
 
     // makes a vector of the position of the spaces
     // goal: use this to check substring between spaces if they contain:
      //  ! . , ; “ ‘ ? $ 
     vector<int> posOfSpace(0);
     posOfSpace.push_back(0);
-    for(int i = 0; i<= lenParagraph; i++){
-        if(paragraph[i] == ' '){
+    for (int i = 0; i <= lenParagraph; i++) {
+        if (paragraph[i] == ' ') {
             posOfSpace.push_back(i);
         }
     }
     posOfSpace.push_back(lenParagraph);
-   
-    for(int i = 0; i<posOfSpace.size() - 1; i++){
-        string tempWord = paragraph.substr(posOfSpace.at(i), (posOfSpace.at(i+1)- posOfSpace.at(i)));
-         char apos = char(39);
-         bool found1 = tempWord.find('!') > 0 && tempWord.find('!') <= tempWord.size();
-         bool found2 = tempWord.find('.') > 0 && tempWord.find('.') <= tempWord.size();
-         bool found3 = tempWord.find(',') > 0 && tempWord.find(',') <= tempWord.size();
-         bool found4 = tempWord.find(';') > 0 && tempWord.find(';') <= tempWord.size();
-         bool found5 = tempWord.find(apos) > 0 && tempWord.find(apos) <= tempWord.size();
-         bool found6 = tempWord.find('?') > 0 && tempWord.find('?') <= tempWord.size();
-        if(found1 || found2 || found3 || found4 || found5 || found6){
-        includeSpecial++;
+
+    // does each substring have a special character?
+    for (int i = 0; i < posOfSpace.size() - 1; i++) {
+        string tempWord = paragraph.substr(posOfSpace.at(i), (posOfSpace.at(i + 1) - posOfSpace.at(i)));
+        char apos = char(39);
+        bool found1 = tempWord.find('!') > 0 && tempWord.find('!') <= tempWord.size();
+        bool found2 = tempWord.find('.') > 0 && tempWord.find('.') <= tempWord.size();
+        bool found3 = tempWord.find(',') > 0 && tempWord.find(',') <= tempWord.size();
+        bool found4 = tempWord.find(';') > 0 && tempWord.find(';') <= tempWord.size();
+        bool found5 = tempWord.find(apos) > 0 && tempWord.find(apos) <= tempWord.size();
+        bool found6 = tempWord.find('?') > 0 && tempWord.find('?') <= tempWord.size();
+        if (found1 || found2 || found3 || found4 || found5 || found6) {
+            includeSpecial++;
         }
     }
-    cout<<"Number of words that contain a special character: "<<includeSpecial<<endl;
+    cout << "Number of words that contain a special character: " << includeSpecial << endl;
 
-    for(int i = 0; i<posOfSpace.size() - 1; i++){
-        string tempWord = paragraph.substr(posOfSpace.at(i), (posOfSpace.at(i+1)- posOfSpace.at(i)));
-        if( toupper(tempWord.at(0)) == 'A' || toupper(tempWord.at(0)) == 'E' || toupper(tempWord.at(0)) == 'I' || toupper(tempWord.at(0)) == 'O' || toupper(tempWord.at(0)) == 'U'){
+    for (int i = 0; i < posOfSpace.size() - 1; i++) {
+        string tempWord = paragraph.substr(posOfSpace.at(i), (posOfSpace.at(i + 1) - posOfSpace.at(i)));
+        if (toupper(tempWord.at(0)) == 'A' || toupper(tempWord.at(0)) == 'E' || toupper(tempWord.at(0)) == 'I' || toupper(tempWord.at(0)) == 'O' || toupper(tempWord.at(0)) == 'U') {
             startWithVowel++;
         }
     }
 
-    for(int i = 0; i <lenParagraph; i++){
+    for (int i = 0; i < lenParagraph; i++) {
         char letter = paragraph[i];
-        switch(tolower(letter))
+        switch (tolower(letter))
         {
-            case 'a': vowels++; break;
-            case 'e': vowels++; break;
-            case 'i': vowels++; break;
-            case 'o': vowels++; break;
-            case 'u': vowels++; break;
-            case 'q': consonants++; break;
-            case 'w': consonants++; break;
-            case 'r': consonants++; break;
-            case 't': consonants++; break;
-            case 'y': consonants++; break;
-            case 'p': consonants++; break;
-            case 's': consonants++; break;
-            case 'd': consonants++; break;
-            case 'f': consonants++; break;
-            case 'g': consonants++; break;
-            case 'h': consonants++; break;
-            case 'j':consonants++; break;
-            case 'k': consonants++; break;
-            case 'l': consonants++; break;
-            case 'z': consonants++; break;
-            case 'x': consonants++; break;
-            case 'c': consonants++; break;
-            case 'v': consonants++; break;
-            case 'b': consonants++; break;
-            case 'n': consonants++; break;
-            case 'm': consonants++; break;
+        case 'a': vowels++; break;
+        case 'e': vowels++; break;
+        case 'i': vowels++; break;
+        case 'o': vowels++; break;
+        case 'u': vowels++; break;
+        case 'q': consonants++; break;
+        case 'w': consonants++; break;
+        case 'r': consonants++; break;
+        case 't': consonants++; break;
+        case 'y': consonants++; break;
+        case 'p': consonants++; break;
+        case 's': consonants++; break;
+        case 'd': consonants++; break;
+        case 'f': consonants++; break;
+        case 'g': consonants++; break;
+        case 'h': consonants++; break;
+        case 'j':consonants++; break;
+        case 'k': consonants++; break;
+        case 'l': consonants++; break;
+        case 'z': consonants++; break;
+        case 'x': consonants++; break;
+        case 'c': consonants++; break;
+        case 'v': consonants++; break;
+        case 'b': consonants++; break;
+        case 'n': consonants++; break;
+        case 'm': consonants++; break;
         }
     }
-    cout<<"Number of vowels: "<<vowels<<endl;
-    cout<<"Number of consonants: "<<consonants<<endl;
+    cout << "Number of vowels: " << vowels << endl;
+    cout << "Number of consonants: " << consonants << endl;
 
     int lengthUser;
-    cout<<"Please type a number, and I will return all words you typed that are longer than that number: ";
-    cin>>lengthUser;
+    cout << "Please type a number, and I will return all words you typed that are longer than that number: ";
+    cin >> lengthUser;
 
-    for(int i = 0; i<posOfSpace.size() - 1; i++){
-        string tempWord = paragraph.substr(posOfSpace.at(i), (posOfSpace.at(i+1)-posOfSpace.at(i)));
-        if(tempWord.size() > lengthUser + 1){
-            cout<<tempWord<< endl;
+    for (int i = 0; i < posOfSpace.size() - 1; i++) {
+
+        if (i == 0) {
+            string tempWord = paragraph.substr(0, (posOfSpace.at(1)));
+            if (tempWord.size() > lengthUser) {
+                cout << tempWord << endl;
+            }
+        }
+        else {
+            string tempWord = paragraph.substr(posOfSpace.at(i), (posOfSpace.at(i + 1) - posOfSpace.at(i)));
+            if (tempWord > lengthUser) {
+                cout << tempWord << endl;
+            }
         }
     }
-    srand((unsigned) time(NULL));
+
+    srand((unsigned)time(NULL));
     int randomNum = 6 + rand() % 5;
-    cout<<"Generating a random number from 6-10 which will download a virus onto your computer: "<< randomNum<<endl;
-    for(int i = 0; i<posOfSpace.size() - 1; i++){
-        string tempWord = paragraph.substr(posOfSpace.at(i), (posOfSpace.at(i+1)-posOfSpace.at(i)));
-        if(tempWord.size() < randomNum){
-            cout<<tempWord<< endl;
+    cout << "Generating a random number from 6-10 which will download a virus onto your computer: " << randomNum << endl;
+    for (int i = 0; i < posOfSpace.size() - 1; i++) {
+        string tempWord = paragraph.substr(posOfSpace.at(i), (posOfSpace.at(i + 1) - posOfSpace.at(i)));
+        if (tempWord.size() < randomNum) {
+            cout << tempWord << endl;
         }
     }
     return 0;
