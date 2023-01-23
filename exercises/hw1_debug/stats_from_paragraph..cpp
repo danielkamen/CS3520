cef54cca7dba7248a948ddd3877eb89a5415fac4
danielkamen <kamen.d@northeastern.edu>
2023-01-23 13:37:30 -0500

    fixed paragraph file to work with vectors

--- exercises/hw1_debug/stats_from_paragraph..cpp
+++ exercises/hw1_debug/stats_from_paragraph..cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include<string>
 #include <vector>
-#include<cstdlib>
+#include <cstdlib>
 
 using namespace std;
 
@@ -11,55 +11,72 @@ int main() {
     string paragraph;
     int words = 0, consonants = 0, vowels = 0, includeSpecial = 0, startWithVowel = 0;
 
+    // paragraph input data
     cout << "Please enter a paragraph. Press [Enter] when you are done." << endl;
     getline(cin, paragraph);
     if (paragraph.empty()) {
         cout << "Error! Paragraph name is required.\n";
     }
+
+    // determines how many words are there.
     int lenParagraph = paragraph.size();
-    cout << "What you typed:" << paragraph << endl;
+    cout << "What you typed: " << paragraph << endl;
     for (int i = 0; i <= lenParagraph; i++) {
         if (paragraph[i] == ' ') {
             words++;
         }
     }
+    // adds count for last word
     words++;
     cout << "Number of words typed: " << words << endl;
 
     // makes a vector of the position of the spaces
     // goal: use this to check substring between spaces if they contain:
      //  ! . , ; “ ‘ ? $ 
-    vector<int> posOfSpace(0);
+    vector<int> posOfSpace;
     posOfSpace.push_back(0);
     for (int i = 0; i <= lenParagraph; i++) {
         if (paragraph[i] == ' ') {
             posOfSpace.push_back(i);
         }
     }
+    // adds an index at the end indicating a space after the last word (placeholder used to get the last word)
     posOfSpace.push_back(lenParagraph);
 
+    // makes a vector of the words
+    // gets a substring between the spaces(recored in vector posOfSpaces)
+    vector<string> wordsVec;
+        for (int i = 0; i < posOfSpace.size() - 1; i++) {
+            if (i == 0) {
+                wordsVec.push_back(paragraph.substr(0, (posOfSpace.at(i + 1))));
+            }
+            else {
+            wordsVec.push_back(paragraph.substr(posOfSpace.at(i) + 1, (posOfSpace.at(i + 1) - posOfSpace.at(i))));
+            }
+        }
+
     // does each substring have a special character?
-    for (int i = 0; i < posOfSpace.size() - 1; i++) {
-        string tempWord = paragraph.substr(posOfSpace.at(i), (posOfSpace.at(i + 1) - posOfSpace.at(i)));
+    for (int i = 0; i < wordsVec.size(); i++) {
         char apos = char(39);
-        bool found1 = tempWord.find('!') > 0 && tempWord.find('!') <= tempWord.size();
-        bool found2 = tempWord.find('.') > 0 && tempWord.find('.') <= tempWord.size();
-        bool found3 = tempWord.find(',') > 0 && tempWord.find(',') <= tempWord.size();
-        bool found4 = tempWord.find(';') > 0 && tempWord.find(';') <= tempWord.size();
-        bool found5 = tempWord.find(apos) > 0 && tempWord.find(apos) <= tempWord.size();
-        bool found6 = tempWord.find('?') > 0 && tempWord.find('?') <= tempWord.size();
+        bool found1 = wordsVec[i].find('!') > 0 && wordsVec[i].find('!') <= wordsVec[i].size();
+        bool found2 = wordsVec[i].find('.') > 0 && wordsVec[i].find('.') <= wordsVec[i].size();
+        bool found3 = wordsVec[i].find(',') > 0 && wordsVec[i].find(',') <= wordsVec[i].size();
+        bool found4 = wordsVec[i].find(';') > 0 && wordsVec[i].find(';') <= wordsVec[i].size();
+        bool found5 = wordsVec[i].find(apos) > 0 && wordsVec[i].find(apos) <= wordsVec[i].size();
+        bool found6 = wordsVec[i].find('?') > 0 && wordsVec[i].find('?') <= wordsVec[i].size();
         if (found1 || found2 || found3 || found4 || found5 || found6) {
             includeSpecial++;
         }
     }
     cout << "Number of words that contain a special character: " << includeSpecial << endl;
 
-    for (int i = 0; i < posOfSpace.size() - 1; i++) {
-        string tempWord = paragraph.substr(posOfSpace.at(i), (posOfSpace.at(i + 1) - posOfSpace.at(i)));
-        if (toupper(tempWord.at(0)) == 'A' || toupper(tempWord.at(0)) == 'E' || toupper(tempWord.at(0)) == 'I' || toupper(tempWord.at(0)) == 'O' || toupper(tempWord.at(0)) == 'U') {
+    for (int i = 0; i < wordsVec.size() - 1; i++) {
+        if (toupper(wordsVec[i].at(0)) == 'A' || toupper(wordsVec[i].at(0)) == 'E' || toupper(wordsVec[i].at(0)) == 'I' || toupper(wordsVec[i].at(0)) == 'O' || toupper(wordsVec[i].at(0)) == 'U') {
             startWithVowel++;
         }
     }
+
+    cout << "Number of words that start with a vowel: " << startWithVowel << endl;
 
     for (int i = 0; i < lenParagraph; i++) {
         char letter = paragraph[i];
@@ -81,7 +98,7 @@ int main() {
         case 'f': consonants++; break;
         case 'g': consonants++; break;
         case 'h': consonants++; break;
-        case 'j':consonants++; break;
+        case 'j': consonants++; break;
         case 'k': consonants++; break;
         case 'l': consonants++; break;
         case 'z': consonants++; break;
@@ -100,18 +117,16 @@ int main() {
     cout << "Please type a number, and I will return all words you typed that are longer than that number: ";
     cin >> lengthUser;
 
-    for (int i = 0; i < posOfSpace.size() - 1; i++) {
+    for (int i = 0; i < wordsVec.size(); i++) {
 
         if (i == 0) {
-            string tempWord = paragraph.substr(0, (posOfSpace.at(1)));
-            if (tempWord.size() > lengthUser) {
-                cout << tempWord << endl;
+            if (wordsVec[i].size() > lengthUser + 1) {
+                cout << wordsVec[i] << endl;
             }
         }
         else {
-            string tempWord = paragraph.substr(posOfSpace.at(i), (posOfSpace.at(i + 1) - posOfSpace.at(i)));
-            if (tempWord > lengthUser) {
-                cout << tempWord << endl;
+            if (wordsVec[i].size() > lengthUser + 1) {
+                cout << wordsVec[i] << endl;
             }
         }
     }
@@ -119,10 +134,9 @@ int main() {
     srand((unsigned)time(NULL));
     int randomNum = 6 + rand() % 5;
     cout << "Generating a random number from 6-10 which will download a virus onto your computer: " << randomNum << endl;
-    for (int i = 0; i < posOfSpace.size() - 1; i++) {
-        string tempWord = paragraph.substr(posOfSpace.at(i), (posOfSpace.at(i + 1) - posOfSpace.at(i)));
-        if (tempWord.size() < randomNum) {
-            cout << tempWord << endl;
+    for (int i = 0; i < wordsVec.size(); i++) {
+        if (wordsVec[i].size() < randomNum) {
+            cout << wordsVec[i] << endl;
         }
     }
     return 0;
