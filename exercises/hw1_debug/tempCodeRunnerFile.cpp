fbd873266b306f841c378505713a1db30dc4ef44
danielkamen <kamen.d@northeastern.edu>
2023-01-25 10:27:41 -0500

    updated vscode to reflect changes i made in khoury

--- exercises/hw1_debug/tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-
-
-    //delcares variable
-    string paragraph;
-    int words = 0, consonants = 0, vowels = 0, includeSpecial = 0, startWithVowel = 0;
-
-    // paragraph input data
-    cout << "Please enter a paragraph. Press [Enter] when you are done. PLEASE DONT USE A DOUBLE SPACE :)" << endl;
