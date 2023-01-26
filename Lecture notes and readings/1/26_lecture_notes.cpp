3dee122e72410d9cbed4ef0b49856d25b2bde2e3
danielkamen <kamen.d@northeastern.edu>
2023-01-26 12:31:13 -0500

    work

---
+++ Lecture notes and readings/1/26_lecture_notes.cpp
@@ -0,0 +1,30 @@
+/*
+input validation:
+input is what you expect it to be*/
+#include <bitset>
+#include <iomainp>
+#include <iostream>
+#include <limits>
+using namespace std;
+
+int main() {
+int x;
+if (cin>>x) // EXTRACTION OPERATOR
+    cout<<x<<endl;
+    else
+    cout<<"input error"<<endl; // INSERTION OPERATOR
+
+
+
+/*
+istream::operator>>
+return type is an istream referance
+another way of saying when u call cin>>x, compile converts that into a function call that looks like this: cin& operator>>(cin& , x)
+                return cin referance  funtion name
+*/
+int y;
+cin>>y;
+bit
+
+
+}
