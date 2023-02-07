09e43123bb63f2273fff3646a5e61be5aa76ed0c
Daniel Kamen <dsk3110@yahoo.com>
2023-02-06 23:22:58 -0500

    hw2-3 almost done

--- hw2/Problem_3/main.cpp
+++ hw2/Problem_3/main.cpp
@@ -19,36 +19,40 @@ public:
         if (sizes < min || sizes >= max) {
             cout << "Hey, you can't make a " << sizes << "x" << sizes << " maze. Call this method again with a size between" << min << " and " << max << "." << endl;
         }
-        
+
         else {
             vector<vector<string> > board;
             for (int i = 0; i < sizes; i++) {
-              vector<string> row;
+                vector<string> row;
                 for (int j = 0; j < sizes; j++) {
-                    if (i == 0) {
-                        row.push_back("1");
+                    if (i == 0 || i == sizes - 1) {
+                        row.push_back(border);
                     }
-                    else  if (j == sizes - 1) {
-                        row.push_back("2");
+                    else  if (j == 0 || j == sizes - 1) {
+                        row.push_back(border);
                     }
                     else if (j < sizes - 1) {
                         //char value that gets added
                         string spot;
                         int pathOrBorder = (rand() % 2) + 1;
                         if (pathOrBorder == 1) {
-                            row.push_back("3");
+                            row.push_back(border);
                         }
                         else {
-                             row.push_back("4");
+                            row.push_back(path);
                         }
 
                     }
 
 
                 }
-              board.push_back(row);
+                board.push_back(row);
             }
-          board[8][8]="121314";
+            // assigns an enterance and exit value
+            int randomEnterance = rand() % sizes;
+            int randomExit = rand() % sizes;
+            board[randomEnterance][0] = path;
+            board[randomExit][sizes - 1] = path;
             mazeMade = board;
         }
     }
@@ -59,10 +63,10 @@ public:
         for (int i = 0; i < maze.size(); i++) {
             for (int j = 0; j < maze[i].size(); j++) {
                 if (j == maze[i].size() - 1) {
-                    cout << (maze[j][j]) + "\n" << endl;
+                    cout << (maze[i][j]) << endl;
                 }
                 else {
-                    cout << (maze[j][j]) + " ";
+                    cout << (maze[i][j]) + " ";
                 }
             }
         }
@@ -78,5 +82,33 @@ int main() {
     first.path = "-";
     first.mazeGenerator(inputNum);
     first.print(first.mazeMade);
+
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    MazeGenerator second;
+    second.border = "#";
+    second.path = "-";
+    second.mazeGenerator(15);
+    second.print(second.mazeMade);
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    MazeGenerator third;
+    third.border = "#";
+    third.path = "-";
+    third.mazeGenerator(15);
+    third.print(third.mazeMade);
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    MazeGenerator fourth;
+    fourth.border = "#";
+    fourth.path = "-";
+    fourth.mazeGenerator(15);
+    fourth.print(fourth.mazeMade);
+    cout << endl;
+    cout << endl;
+    cout << endl;
 
 }
