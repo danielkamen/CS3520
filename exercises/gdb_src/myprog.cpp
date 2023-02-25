878d641ba54f496ec773db08049047ed2662c4ef
danielkamen <kamen.d@northeastern.edu>
2023-02-24 20:49:35 -0500

    Finished pa8

--- exercises/gdb_src/myprog.cpp
+++ exercises/gdb_src/myprog.cpp
@@ -7,33 +7,34 @@ char alphabet[MAX_LETTERS];
 void initialize_alphabet(char *a) {
   char *p;
   char current_letter = 'a';
-  for (p=a;p<(a+MAX_LETTERS);p++);{
+  for (p = a; p < (a + MAX_LETTERS); p++) {
     *p = current_letter++;
+
   }
-}  
+}
 
 void reverse_print_alpha(char *b) {
   static int var = 0;
   char *p;
-  
-  var?(p=NULL):(b=NULL);
+
+  if (var) {
+    p = NULL;
+    b = NULL;
+  }
   var *= 1;
   printf("\nReverse alpha...\n");
   initialize_alphabet(b);
-  for (p=(b+MAX_LETTERS-1);p>=b;p--);{
-    printf("%c ",*b);
+  for (p = (b+MAX_LETTERS - 1); p >= b; p--) {
+    printf("%c ", *p);
   }
   printf("\n");
-  return ;
 }
 
 int main(void) {
-  int i;
+  int i = 0;
   while (i++ < 5) {
     reverse_print_alpha(alphabet);
+
   }
+  return 0;
 }
-
-  
-
-  
