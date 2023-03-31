9467af499bbdaf3a010ee72f5e2aa063c3851573
danielkamen <kamen.d@northeastern.edu>
2023-03-31 19:30:41 -0400

    pa12 done

--- exercises/pa12/pa12.cpp
+++ exercises/pa12/pa12.cpp
@@ -1,61 +1,58 @@
-#include <iostream>
 #include <cstring>
+#include <iostream>
 #include <memory>
 using namespace std;
 
 class MyString {
-    unique_ptr<char[]> str;
+  unique_ptr<char[]> str;
 
 public:
-    MyString() : str(new char[1]) { // Default Constructor
-        str[0] = '\0';
+  // Default Constructor
+  MyString() : str(new char[1]) {
+    str[0] = '\0';
+    cout << "Default Constructor" << endl;
+  }
+
+  // constructor with a char argument
+  MyString(unique_ptr<char[]> &val) : str(move(val)) {
+    if (val == nullptr) {
+      cout << "The given string is: " << str.get() << endl;
+      str.reset(new char[1]);
+      str[0] = '\0';
     }
+  }
 
+  // constructor that copys its source
+  MyString(const MyString &source) {
+    int length = strlen(source.str.get()) + 1;
+    str.reset(new char[length]);
+    strcpy(str.get(), source.str.get());
+    str[length - 1] = '\0';
+  }
 
- MyString(unique_ptr<char[]>& val) : str(move(val)) {
-        if (val == nullptr) {
-            str.reset(new char[1]);
-            str[0] = '\0';
-        } else {
-            int length = strlen(val.get()) + 1;
-            str.reset(new char[length]);
-            strcpy(str.get(), val.get());
-             str[strlen(val.get())] = '\0';
-            cout << "The given string is: " << str.get() << endl;
-        }
-    }
+  //*******************************************************
+  // THIS WORKS ON REPLIT BUT NOT ON KHOURY SERVER
+  // Move Constructor
+  MyString(MyString &&source) { str = move(source.str); }
+  //******************************************************
 
-
-    MyString(const MyString& source) { // Copy Constructor
-        int length = strlen(source.str.get()) + 1;
-        str.reset(new char[length]);
-        strcpy(str.get(), source.str.get());
-        str[length - 1] = '\0';
-    }
-
-    MyString(MyString&& source) noexcept { // Move Constructor
-        str = move(source.str);
-    }
-
-    ~MyString() = default; // Destructor
+  // Destructor
+  ~MyString() = default;
 };
 
 int main(void) {
 
-//unique_ptr<Test> a(new Test(1));
-//unique_ptr<Test[]> b(new Test[2]{2,3});
-//cout<<"ID:"<<a->get_id()<<endl;
-//cout<<"ID:"<<b[0].get_id()<<endl;
+  cout << "Start!" << endl;
+  unique_ptr<MyString> a(new MyString());
+  unique_ptr<char[]> hello(new char[6]{'H', 'e', 'l', 'l', 'o', '\0'});
+  unique_ptr<MyString> b(new MyString(hello));
+  // THIS WORKS ON REPLIT BUT NOT ON KHOURY SERVER
+  //unique_ptr<char[]> hello2(new char[7]{"Hello2"});
+  // THIS WORKS ON REPLIT BUT NOT ON KHOURY SERVER
+  //MyString c{MyString{hello2}};
 
+  unique_ptr<char[]> world(new char[6]{'W', 'o', 'r', 'l', 'd', '\0'});
+  unique_ptr<MyString> d{new MyString(world)};
 
-/*deleting lines 16-19 produces a seg fault*/
-/* line 57, wtf how do i do?*/
-    cout << "Start!" << endl;
-    unique_ptr<MyString> a(new MyString());
-    unique_ptr<char[]> hello(new char[6]{'H', 'e', 'l', 'l', 'o', '\0'});
-    unique_ptr<MyString> b(new MyString(hello));
-    unique_ptr<MyString> c(a);
-   
-
-    cout << "Bye" << endl;
+  cout << "Bye" << endl;
 }
